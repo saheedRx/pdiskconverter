90d066d8d62a2afe0ed54e0df2edf2b7a4dc3717
saheedRx <89293737+saheedRx@users.noreply.github.com>
2021-09-29 09:35:58 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ bot = Client('pdiskshortner bot',
 async def start(bot, message):
     await message.reply(
         f"**Hi {message.chat.first_name}!**\n\n"
-        "I'm Pdisk link converter bot.\n Just send me link and get converted link of pdisk. \n Press 👉 /help 👈 for More. \n\n Created By @Tamil_Rulzz")
+        "I'm Pdisk link converter bot.\nJust send me link and get converted link of pdisk. \n\nPress 👉 /help 👈 for More. \n\n\n Created By @Tamil_Rulzz")
 
 @bot.on_message(filters.command('help') & filters.private)
 async def help(bot, message):
